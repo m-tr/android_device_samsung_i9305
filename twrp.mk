1184c0a903d97b298b764c3adc65145b0bad1681
m-tr <mtrgit@gmail.com>
2020-04-26 22:46:57 +0200

    TWRP changes
    Change-Id: Ic8bbfca24002da44dd9f9dc74acb1712d81d3d9a

--- twrp.mk
+++ twrp.mk
@@ -7,6 +7,12 @@ PRODUCT_COPY_FILES += device/samsung/i9305/rootdir/twrp.fstab:recovery/root/etc/
 TWRP_NEW_THEME := true
 TW_THEME := portrait_hdpi
 TW_INCLUDE_CRYPTO := true
+HAVE_SELINUX := true
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/smdk4412-common/twrp/custombootimg.mk
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
 RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
